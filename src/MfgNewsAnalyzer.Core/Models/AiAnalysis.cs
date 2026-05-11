7e0b0fe0483d44150e8f5137ba5413f9b174ea18
John Muraski <john.t.muraski@outlook.com>
2026-05-11 11:01:29 -0500

    Adding models

---
+++ src/MfgNewsAnalyzer.Core/Models/AiAnalysis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace MfgNewsAnalyzer.Core.Models;
+
+public record AiAnalysis
+{
+    [JsonPropertyName("claudeSummary")]
+    public required string ClaudeSummary { get; init; }
+
+    [JsonPropertyName("recommendation")]
+    public required int Recommendation { get; init; }
+
+    [JsonPropertyName("sentiment")]
+    public required int Semantic { get; init; }
+
+    [JsonPropertyName("tags")]
+    public required IReadOnlyList<string> Tags { get; init; } = new List<string>();
+}
---
+++ src/MfgNewsAnalyzer.Core/Models/Article.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MfgNewsAnalyzer.Core.Models;
+public record Article
+{
+    [JsonPropertyName("id")]
+    public required string Id { get; init; } = Guid.NewGuid().ToString();
+
+    // ---Article Properties---
+
+    // --Required Properties--
+    [JsonPropertyName("publisher")]
+    public required string Publisher { get; init; }
+
+    [JsonPropertyName("url")]
+    public required string Url { get; init; }
+
+    [JsonPropertyName("pulledDate")]
+    public required DateTime PulledDate { get; init; }
+
+    [JsonPropertyName("title")]
+    public required string Title { get; init; }
+
+    // --Optional Properties--
+    [JsonPropertyName("description")]
+    public string? Description { get; init; }
+
+    [JsonPropertyName("author")]
+    public string? Author { get; init; }
+
+    [JsonPropertyName("publishDate")]
+    public DateTime? PublishDate { get; init; }
+
+    [JsonPropertyName("rawText")]
+    public string? RawText { get; init; }
+
+    // ---AI Analysis Properties--
+    [JsonPropertyName("aiAnalysisResults")]
+    public AiAnalysis? AiAnalysisResults { get; init; }
+}
