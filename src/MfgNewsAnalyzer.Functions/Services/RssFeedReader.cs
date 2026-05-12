ff152f3df9834123d6ab90d684faaa81c7cef96a
John Muraski <john.t.muraski@outlook.com>
2026-05-11 19:35:52 -0500

    switching pcs

---
+++ src/MfgNewsAnalyzer.Functions/Services/RssFeedReader.cs
@@ -0,0 +1,19 @@
+﻿using MfgNewsAnalyzer.Core.Abstractions;
+using MfgNewsAnalyzer.Core.Models;
+using CodeHollow.FeedReader;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace MfgNewsAnalyzer.Functions.Services
+{
+    public class RssFeedReader : IRssFeedReader
+    {
+        private readonly RssFeedReaderOptions _options;
+        private readonly ILogger<RssFeedReader> _logger;
+
+        public Task<IReadOnlyList<Article>> ReadFeedAsync(string url, string publisher, CancellationToken canellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
