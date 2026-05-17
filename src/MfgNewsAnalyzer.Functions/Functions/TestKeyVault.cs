8232534edf6f1d8258217f4484f3b691f7657547
John Muraski <john.t.muraski@outlook.com>
2026-05-16 23:08:01 -0500

    chore: testing azure connection

---
+++ src/MfgNewsAnalyzer.Functions/Functions/TestKeyVault.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MfgNewsAnalyzer.Functions.Functions;
+
+public class TestKeyVault
+{
+    private readonly ILogger<TestKeyVault> _logger;
+
+    public TestKeyVault(ILogger<TestKeyVault> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(TestKeyVault))]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
+        CancellationToken cancellationToken)
+    {
+        const string vaultUrl = "https://jts-api-keys.vault.azure.net/";
+        const string secretName = "Anthropoc--MainApiKey";
+
+        _logger.LogInformation("TestKeyVault invoked");
+
+        var credential = new AzureCliCredential();
+        var client = new SecretClient(new Uri(vaultUrl), credential);
+
+        _logger.LogInformation("Attempting to retrieve secret SecretName");
+        var secret = await client.GetSecretAsync(secretName, cancellationToken: cancellationToken);
+
+        var secretPrefix = secret.Value.Value.Length >= 10
+            ? secret.Value.Value.Substring(0, 10) + "..."
+            : "(value shorter than expected)";
+
+        return new OkObjectResult(new
+        {
+            Status = "Successfully retrieved secret",
+            SecretName = secretName,
+            SecretPrefix = secretPrefix,
+            SecretLength = secret.Value.Value.Length
+        });
+    }
+}
--- src/MfgNewsAnalyzer.Functions/Program.cs
+++ src/MfgNewsAnalyzer.Functions/Program.cs
@@ -4,10 +4,28 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+// Key Vault Set up
+Uri vaultUrl = new Uri("https://jts-api-keys.vault.azure.net/");
+var credentials = new DefaultAzureCredential(new DefaultAzureCredentialOptions()
+{
+    ExcludeVisualStudioCredential = true,
+    ExcludeVisualStudioCodeCredential = true,
+    ExcludeSharedTokenCacheCredential = true,
+    ExcludeInteractiveBrowserCredential = true,
+    ExcludeAzurePowerShellCredential = true,
+    ExcludeAzureDeveloperCliCredential = true,
+    ExcludeWorkloadIdentityCredential = true
+});
+
+builder.Configuration.AddAzureKeyVault(vaultUrl, credentials);
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
